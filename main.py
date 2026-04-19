a7539a4aa82462cb0942c3d18ec577a9fa733e8f
devn0n <osoraanwankwo@gmail.com>
2026-04-20 00:01:59 +0100

    feature - add groups

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ def main():
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))	
 	clock = pygame.time.Clock()
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	dt = 0
+ 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	
  
+	
 	while True:
 		log_state()
 		
@@ -23,8 +28,12 @@ def main():
 				return
 
 		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
+		
+		updatable.update(dt)
+		# drawable.draw(screen)
+		for sprite in drawable:
+			sprite.draw(screen)
+   
 		pygame.display.flip()
 		
 		dt = clock.tick(60) / 1000
