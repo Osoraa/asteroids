d9ca49968d698ee549363d984b0974d0b48aa01e
devn0n <osoraanwankwo@gmail.com>
2026-04-20 00:55:45 +0100

    feature - implement asteroids and asteroidfield

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state 
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from logger import log_state
 from player import Player
 
 
 def main():
-	# print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-	# print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-	pygame.init()
+    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
 
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))	
-	clock = pygame.time.Clock()
-	dt = 0
- 
-	updatable = pygame.sprite.Group()
-	drawable = pygame.sprite.Group()
-	Player.containers = (updatable, drawable)
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	
- 
-	
-	while True:
-		log_state()
-		
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-		screen.fill("black")
-		
-		updatable.update(dt)
-		# drawable.draw(screen)
-		for sprite in drawable:
-			sprite.draw(screen)
-   
-		pygame.display.flip()
-		
-		dt = clock.tick(60) / 1000
-		# print(dt) 
 
+    asteroids = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+    
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        updatable.update(dt)
+        # drawable.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        # print(dt)
 
 
 if __name__ == "__main__":
